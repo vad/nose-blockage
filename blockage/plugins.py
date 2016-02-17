05ea97b27b998050ef3a5cce0fc7de245875779f
Davide Setti <davide.setti@gmail.com>
2016-02-17 12:51:59 +0100

    fix basestring in python3

--- blockage/plugins.py
+++ blockage/plugins.py
@@ -2,6 +2,22 @@ try:
     import httplib
 except ImportError:
     import http.client as httplib
+
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str, bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+
 import logging
 import os
 import smtplib
